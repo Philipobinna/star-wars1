35339d5ea4341ac1ed2d77c463f3e25df7a9cac2
philipObinna <philip.obinnakwelu@decagon.dev>
2023-08-21 22:27:06 +0100

    completed star war

--- script/index.js
+++ script/index.js
@@ -1,3 +1,33 @@
-function main() {}
+const characterListElement = document.querySelector('.character-list');
+const characterDetailsElement = document.querySelector('.character-details');
 
-module.exports = { main }
+// Fetch characters from the API
+fetch('https://swapi.dev/api/people')
+    .then(response => response.json())
+    .then(data => {
+        const characters = data.results;
+        displayCharacters(characters);
+    })
+    .catch(error => console.error('Error fetching characters:', error));
+
+function displayCharacters(characters) {
+    characterListElement.innerHTML = '';
+    
+    characters.forEach(character => {
+        const characterCard = document.createElement('div');
+        characterCard.classList.add('character-card');
+        characterCard.innerText = character.name;
+        characterCard.addEventListener('click', () => displayCharacterDetails(character));
+        characterListElement.appendChild(characterCard);
+    });
+}
+
+function displayCharacterDetails(character) {
+    const detailsHTML = `
+        <h2>Name: ${character.name}</h2>
+        <p>Gender: ${character.gender}</p>
+        <p>Height: ${character.height}</p>
+    `;
+    characterDetailsElement.innerHTML = detailsHTML;
+    characterDetailsElement.style.display = 'block';
+}
